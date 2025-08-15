88ea755e4b8937021ae56f8642c1a9c007c4774a
Damian Durnford <damiandurnford@hotmail.com>
2025-08-15 22:02:31 +0800

    Serve static files and improve error handling

--- server.js
+++ server.js
@@ -41,5 +41,9 @@ app.post('/chat', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
